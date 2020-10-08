43c36821828ff22878a75bc2cf297fc1b70723ca
ChristianCuri <contato.mikesantos@gmail.com>
2020-10-08 13:08:51 -0300

    feat: create route to get all states and population by state

--- src/service/module_name/routes.ts
+++ src/service/module_name/routes.ts
@@ -1,4 +1,7 @@
 import express from "express";
 import * as controller from "./controller";
 import { CatchErrors } from "@utils";
-export default express.Router().get("/", CatchErrors(controller.listRoles));
+export default express
+  .Router()
+  .get("/estados", CatchErrors(controller.getStates))
+  .get("/populacao/:uf", CatchErrors(controller.getPopulationByUf));
